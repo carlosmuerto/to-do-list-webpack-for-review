34db7c36cd7dd7c0d81d457b75440fbf8c05db35
Carlos Vivas <carlosmuerto@gmail.com>
2022-08-16 16:01:54 -0400

    remove console.table()

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import './style.css';
 import { library, dom } from '@fortawesome/fontawesome-svg-core';
 
 import {
-  faArrowsRotate, faRightToBracket, faCheck, faEllipsisVertical,
+	faArrowsRotate, faRightToBracket, faCheck, faEllipsisVertical,
 } from '@fortawesome/free-solid-svg-icons';
 
 import ToDoList from './modules/to-do-list.js';
@@ -11,39 +11,37 @@ import { PopulateTaskList, removeTaskElement } from './modules/to-do-dom-element
 const toDoList = new ToDoList();
 
 const removeTaskButtonEvent = (e) => {
-  e.preventDefault();
-  toDoList.getDoneTasks().forEach((task) => {
-    removeTaskElement(task.index);
-  });
-  toDoList.deleteDoneTasks();
+	e.preventDefault();
+	toDoList.getDoneTasks().forEach((task) => {
+		removeTaskElement(task.index);
+	});
+	toDoList.deleteDoneTasks();
 };
 
 const addTaskToTodoList = (e) => {
-  e.preventDefault();
-  const taskInput = document.getElementById('task-input');
-  const task = taskInput.value;
-  taskInput.value = '';
-  toDoList.addToDo(task);
-  PopulateTaskList(toDoList);
+	e.preventDefault();
+	const taskInput = document.getElementById('task-input');
+	const task = taskInput.value;
+	taskInput.value = '';
+	toDoList.addToDo(task);
+	PopulateTaskList(toDoList);
 };
 
 const refreshListBtn = (e) => {
-  e.preventDefault();
-  // for debuging onlly
-  // eslint-disable-next-line no-console
-  console.table(toDoList.tasks);
+	e.preventDefault();
+	// Botton free for anything
 };
 
 const init = () => {
-  toDoList.init();
-  PopulateTaskList(toDoList);
-  document.getElementById('clear-task-btn').addEventListener('click', removeTaskButtonEvent);
-  document.getElementById('add-task-form').addEventListener('submit', addTaskToTodoList);
-  document.getElementById('refresh-btn').addEventListener('click', refreshListBtn);
+	toDoList.init();
+	PopulateTaskList(toDoList);
+	document.getElementById('clear-task-btn').addEventListener('click', removeTaskButtonEvent);
+	document.getElementById('add-task-form').addEventListener('submit', addTaskToTodoList);
+	document.getElementById('refresh-btn').addEventListener('click', refreshListBtn);
 
-  // Kicks off the process of finding <i> tags and replacing with <svg>
-  library.add(faArrowsRotate, faRightToBracket, faCheck, faEllipsisVertical);
-  dom.watch();
+	// Kicks off the process of finding <i> tags and replacing with <svg>
+	library.add(faArrowsRotate, faRightToBracket, faCheck, faEllipsisVertical);
+	dom.watch();
 };
 
 window.addEventListener('load', init);
